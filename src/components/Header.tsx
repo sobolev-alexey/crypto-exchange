426d3a6080f39d238b17ef47789fdfc14e94afa0
Alexey Sobolev <sobolev@mail.de>
2022-10-08 01:30:17 +0200

    feat(App): Header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import logo from '../assets/header/logo.svg';
+import logoMobile from '../assets/logo.png';
+import logo from '../assets/logo_long.png';
 import { Search } from '.';
 
 const Header = () => {
   return (
     <header className="header-wrapper">
-      <div className="header-middle-wrapper">
-        <div className="header-content">
-          {/* <Link to="/"><img src={logo} alt="Crypto Exchange Directory" className="logo"/></Link> */}
-          <Search />
-        </div>
+      <div className="header-content">
+        <Link to="/">
+          <img src={logo} alt="Crypto Exchange Directory" className="logo" />
+          {/* <img src={logoMobile} alt="Crypto Exchange Directory" className="logo-mobile" /> */}
+        </Link>
+        <Search />
       </div>
     </header>
   );
