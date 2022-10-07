d90a8220fcb3643c330b3206d1dc8ffd40fc73d1
Alexey Sobolev <sobolev@mail.de>
2022-10-08 01:30:28 +0200

    feat(App): Footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo_long.png';
 
 const Footer = () => {
   return (
     <footer className="footer-wrapper">
-      <div className="footer-middle-wrapper">
-        <div className="footer-content">
-          Footer
+      <div className="footer-content">
+        <Link to="/">
+          <img src={logo} alt="Crypto Exchange Directory" className="logo" />
+        </Link>
+        <div className="copyright">
+          <a href="https://lexer.dev" target="_blank" rel="noopener noreferrer">
+            © Alexey Sobolev (https://lexer.dev)
+          </a>
+          <span>
+            Data provided by 
+            <a href="https://www.coingecko.com/" target="_blank" rel="noopener noreferrer">
+              &nbsp;CoinGecko
+            </a>
+          </span>
         </div>
       </div>
     </footer>
