04af2f894d1b344515bb51767dee7eee4c45ca2b
Alexey Sobolev <sobolev@mail.de>
2022-10-07 14:51:38 +0200

    feat(App): Common reusable components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,13 @@
+import Header from './Header';
+import Loading from './Loading';
 import ErrorFallback from './ErrorFallback';
+import ExchangesTable from './ExchangesTable';
+import VolumeChart from './VolumeChart';
 
 export {
+  Header,
+  Loading,
   ErrorFallback,
+  ExchangesTable,
+  VolumeChart,
 }
