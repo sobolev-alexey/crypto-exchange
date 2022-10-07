5e8d769fc2a6d88aedc21abe5529023bba7cfa31
Alexey Sobolev <sobolev@mail.de>
2022-10-07 20:13:18 +0200

    feat(App): Details page composition

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import useSWR from 'swr';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { Avatar } from 'antd';
+import { useParams } from 'react-router-dom';
+import { Markets, ExchangeLinks } from '../components';
+import { AppContext } from '../context/globalState';
+import { formatPrice, formatVolume, convertColor } from '../utils/format';
 
 const Details = () => {
+  const { priceBTC }: { priceBTC: number } = useContext(AppContext);
   const { id } = useParams();
-  const navigate = useNavigate();
   const { data: exchange, error } = useSWR(`${import.meta.env.VITE_API_BASE}/exchanges/${id}`);
+  
+  console.log('Exchange data', id, exchange)
+
   return (
-    <div>
-      Exchange Data {JSON.stringify(exchange)}
-      Error {JSON.stringify(error)}
-      <Link to='/'>Back</Link>
+    <div className="details-wrapper">
+      <div className="details-main-content-wrapper">
+        <div className="exchange-details-wrapper">
+          <div className="exchange-name-wrapper">
+            <Avatar
+              size={40}
+              alt={exchange?.name}
+              src={exchange?.image}
+            />
+            {exchange?.name}
+          </div>
+          <ExchangeLinks exchange={exchange} />
+        </div>
+        <div className="exchange-stats-wrapper">
+          <div className="exchange-volume-wrapper">
+            <p className="hint">Volume(24h)</p>
+            <h2 className="price">{formatPrice(priceBTC * exchange?.trade_volume_24h_btc || 0)}</h2>
+            <p className="hint">{formatVolume(exchange?.trade_volume_24h_btc || 0)} BTC</p>
+          </div>
+          <div className="exchange-stats">
+            <div className="exchange-score">
+              <h2 style={{ color: convertColor(exchange?.trust_score) }}>
+                {exchange?.trust_score}
+              </h2>
+              Score
+            </div>
+          </div>
+        </div>
+      </div>
+      <Markets tickers={exchange?.tickers} />
     </div>
   );
 };
