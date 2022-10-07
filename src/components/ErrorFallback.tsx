684036b5789f6f445650c451e2b3e6c46fac8e04
Alexey Sobolev <sobolev@mail.de>
2022-10-07 14:22:05 +0200

    feat(Error): Advanced error notification using Antd

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -1,10 +1,25 @@
+import React from 'react';
+import { Result } from 'antd';
+// import { FallbackProps } from 'react-error-boundary';
+
 const ErrorFallback = ({ error, resetErrorBoundary }) => {  // { error, resetErrorBoundary } : FallbackProps
   console.error(error);
 
+  const Home = () => (
+    <a href="/" className="primary">
+      <button onClick={resetErrorBoundary} className="primary">
+        Back Home
+      </button>
+    </a>
+  );
+
   return (
-    <div>
-      Error
-    </div>
+    <Result
+      status="500"
+      title="500"
+      subTitle="Sorry, something went wrong."
+      extra={<Home />}
+    />
   );
 };
 
