2d6fc777645c9e3ce74c9f9bcd1933f3449a46b6
Alexey Sobolev <sobolev@mail.de>
2022-10-08 22:35:14 +0200

    chore(App): ESSLint, Prettier config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   preview: {
-    port: 3000
+    port: 3000,
   },
   server: {
-    port: 3000
+    port: 3000,
   },
-})
+});
