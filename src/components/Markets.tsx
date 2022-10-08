9a5076654103913d64c58039c4b58af2575e5c09
Alexey Sobolev <sobolev@mail.de>
2022-10-08 18:58:26 +0200

    style(App): Details page responsive styling

--- src/components/Markets.tsx
+++ src/components/Markets.tsx
@@ -1,77 +1,88 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Table } from 'antd';
 import { formatPrice, formatPercentage } from '../utils/format';
+import useWindowDimensions from '../utils/useWindowDimensions';
 import { Ticker } from '../types';
 
-const Markets = ({ tickers, viewport = '' }: { tickers: Ticker[], viewport?: string }) => {
+const Markets = ({ tickers }: { tickers: Ticker[] }) => {
+  const { viewport, screenWidth } = useWindowDimensions();
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
 
+  console.log('viewport', viewport)
   const columns = [
     {
       title: '#',
       dataIndex: 'id',
       key: 'id',
-      render: (id: string | number, record: Ticker, index: number) => {
-         ++index; 
-         return index; 
-      },
+      render: (id: any, record: Ticker, index: number) => ++index,
       showSorterTooltip: false,
+      fixed: viewport === 'mobile',
       width: 50,
     },
     {
       title: 'Pair',
       key: 'coin_id',
       render: (record: Ticker) => `${record?.base} / ${record?.target}`,
-      width: ['mobileLandscape', 'mobilePortrait'].includes(viewport) ? 150 : 175,
+      fixed: viewport === 'mobile',
+      width: viewport === 'mobile' ? 90 : 175,
     },
     {
       title: 'Price',
       key: 'last',
       render: (record: Ticker) => formatPrice(record?.last || 0),
-      width: 150,
+      width: viewport === 'mobile' ? 90 : 150,
     },
     {
       title: 'Spread',
       key: 'bid_ask_spread_percentage',
       render: (record: Ticker) => formatPercentage(record?.bid_ask_spread_percentage || 0),
-      width: 150,
+      width: viewport === 'mobile' ? 70 : 150,
     },
     {
       title: 'Volume (24h)',
       key: 'volume',
       render: (record: Ticker) => formatPrice((record?.volume || 0) * (record?.last || 0)),
-      width: 200,
+      width: viewport === 'mobile' ? 120 : 200,
     },
   ];
 
   return (
-    <Table
-      scroll={{ x: 900 }}
-      pagination={{
-        showSizeChanger: true,
-        defaultPageSize: 25,
-        pageSizeOptions: [10, 25, 50, 100],
-        onShowSizeChange: (current, size) => {
-          setPage(current);
-          setPageSize(size)
-        },
-        onChange: (page, pageSize) => {
-          setPage(page);
-          setPageSize(pageSize)
-        },
-        showTotal: (total, range) => {
-          let pageCount = Math.ceil(total / pageSize);
-          if (total === 0 && pageCount === 0) {
-            pageCount = 1;
+    <React.Fragment>
+      <Table
+        scroll={{ x: viewport === 'mobile' ? Math.max(screenWidth, 600) : 900 }}
+        pagination={{
+          showSizeChanger: true,
+          defaultPageSize: 25,
+          pageSizeOptions: [10, 25, 50, 100],
+          onShowSizeChange: (current, size) => {
+            setPage(current);
+            setPageSize(size)
+          },
+          onChange: (page, pageSize) => {
+            setPage(page);
+            setPageSize(pageSize)
+          },
+          showTotal: (total, range) => {
+            let pageCount = Math.ceil(total / pageSize);
+            if (total === 0 && pageCount === 0) {
+              pageCount = 1;
+            }
+            return `${page} of ${pageCount} page${pageCount > 1 ? 's' : ''}`;
           }
-          return `${page} of ${pageCount} page${pageCount > 1 ? 's' : ''}`;
-        }
-      }}
-      dataSource={tickers}
-      columns={columns}
-      rowKey={(record: Ticker) => `${record?.base}/${record?.target}`}
-    />
+        }}
+        dataSource={tickers}
+        columns={columns}
+        rowKey={(record: Ticker) => `${record?.base}/${record?.target}`}
+      />
+      {
+        screenWidth < 600 && (
+          <span className="table-scroll-hint">
+            {'Scroll sideways to view more data -->'}
+          </span>
+        )
+      }
+    </React.Fragment>
   )
 };
 
