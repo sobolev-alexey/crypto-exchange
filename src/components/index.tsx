7e8016edaeb80b0107c99c54d3817a5927837098
Alexey Sobolev <sobolev@mail.de>
2022-10-10 17:14:14 +0200

    refactor(Add): Refactored exchange data

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -7,6 +7,7 @@ import ExchangesTable from './home/ExchangesTable';
 import Markets from './details/Markets';
 import VolumeChart from './home/VolumeChart';
 import ExchangeLinks from './details/ExchangeLinks';
+import ExchangeData from './details/ExchangeData';
 
 export {
   Header,
@@ -18,4 +19,5 @@ export {
   VolumeChart,
   Markets,
   ExchangeLinks,
+  ExchangeData,
 };
--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,10 +1,8 @@
 import { useContext } from 'react';
 import useSWR from 'swr';
-import { Avatar } from 'antd';
 import { useParams } from 'react-router-dom';
-import { Markets, ExchangeLinks } from '../components';
+import { Markets, ExchangeData } from '../components';
 import { AppContext } from '../context/globalState';
-import { formatPrice, formatVolume, convertColor } from '../utils/format';
 
 const Details = () => {
   const { priceBTC }: { priceBTC: number } = useContext(AppContext);
@@ -13,30 +11,7 @@ const Details = () => {
 
   return (
     <div className='details-wrapper'>
-      <div className='details-main-content-wrapper'>
-        <div className='exchange-details-wrapper'>
-          <div className='exchange-name-wrapper'>
-            <Avatar size={45} alt={exchange?.name} src={exchange?.image} />
-            <h2>{exchange?.name}</h2>
-          </div>
-          <ExchangeLinks exchange={exchange} />
-        </div>
-        <div className='exchange-stats-wrapper'>
-          <div className='exchange-volume-wrapper'>
-            <p className='hint'>Volume(24h)</p>
-            <h1 className='price'>{formatPrice(priceBTC * exchange?.trade_volume_24h_btc || 0)}</h1>
-            <p className='hint'>{formatVolume(exchange?.trade_volume_24h_btc || 0)} BTC</p>
-          </div>
-          <div className='exchange-stats'>
-            <div className='exchange-score'>
-              <h2 style={{ color: convertColor(exchange?.trust_score) }}>
-                {exchange?.trust_score}
-              </h2>
-              Trust Score
-            </div>
-          </div>
-        </div>
-      </div>
+      <ExchangeData exchange={exchange} price={priceBTC} />
       <h3 className='markets-label'>Markets</h3>
       <Markets tickers={exchange?.tickers} />
     </div>
